ba7a41991e75f1f540d8810427671522d4af5cdc
JavierZabala <javier_bislip@hotmail.com>
2019-01-24 13:44:02 -0400

    Authentication added

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,32 +1,45 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import classes from "./Home.css";
 
-const Home = () => {
-  return (
-    <div className={classes.Home}>
-      <div className={classes.Overlay}>
-        <div>
-          <p>
-            JZ Music Player
-            <br />
-            Listen to your favorite songs for free...
-            <br />
-            More than 3500 songs available.
-          </p>
+class Home extends Component {
+  componentDidMount() {
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push("/music");
+    }
+  }
+  render() {
+    return (
+      <div className={classes.Home}>
+        <div className={classes.Overlay}>
           <div>
-
-          <Link to="/register" className={classes.Register}>
-            Register Now
-          </Link>
-          <Link to="/login" className={classes.Login}>
-            Login
-          </Link>
+            <p>
+              JZ Music Player
+              <br />
+              Listen to your favorite songs for free...
+              <br />
+              More than 3500 songs available.
+            </p>
+            <div>
+              <Link to="/register" className={classes.Register}>
+                Register Now
+              </Link>
+              <Link to="/login" className={classes.Login}>
+                Login
+              </Link>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.isAuthenticated
+  };
 };
 
-export default Home;
+export default connect(mapStateToProps)(Home);
