9d999dd45508549be2dfc27dad66118f527b8ee7
JavierZabala <javier_bislip@hotmail.com>
2019-04-25 10:43:27 -0400

    Redux

--- client/src/store.js
+++ client/src/store.js
@@ -2,10 +2,7 @@ import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
-let composeEnhancers =
-  process.env.NODE_ENV === "development"
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    : null;
+let composeEnhancers =window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__|| null;
 if (!composeEnhancers) composeEnhancers = compose;
 
 const initialState = {};
