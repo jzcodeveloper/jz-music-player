985b57c76bf31949dd0600c8294103534808475f
Javier <javier_bislip@hotmail.com>
2019-06-12 10:17:40 -0400

    Added react hook to player playlist

--- client/src/containers/Player/Playlist/Playlist.js
+++ client/src/containers/Player/Playlist/Playlist.js
@@ -1,36 +1,32 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import classes from "./Playlist.css";
 import background from "../../../assets/background.jpg";
 
-class Playlist extends Component {
-  componentDidMount() {
-    this.props.toggleActiveClass();
-  }
+const Playlist = props => {
+  useEffect(() => {
+    props.toggleActiveClass();
+  }, []);
 
-  render() {
-    return (
-      <ul className={classes.Playlist}>
-        {this.props.playlist.map((song, index) => (
-          <li key={index} onClick={() => this.props.onClick(index)}>
-            <img
-              src={
-                song.albumArt !== ""
-                  ? require(`../../../assets/albumArts/${
-                      song.albumArt.albumArt
-                    }`)
-                  : background
-              }
-              alt="Album Art"
-            />
-            <div>
-              <span>{song.title}</span>
-              <span>{song.artist}</span>
-            </div>
-          </li>
-        ))}
-      </ul>
-    );
-  }
-}
+  return (
+    <ul className={classes.Playlist}>
+      {props.playlist.map((song, index) => (
+        <li key={index} onClick={() => props.onClick(index)}>
+          <img
+            src={
+              song.albumArt !== ""
+                ? require(`../../../assets/albumArts/${song.albumArt.albumArt}`)
+                : background
+            }
+            alt="Album Art"
+          />
+          <div>
+            <span>{song.title}</span>
+            <span>{song.artist}</span>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 export default Playlist;
