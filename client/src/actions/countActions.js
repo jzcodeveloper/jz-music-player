9f19c72aa1498c4e2f4f61b5e960fc2182c39407
Javier <javier_bislip@hotmail.com>
2019-07-12 12:22:29 -0400

    Routes

--- client/src/actions/countActions.js
+++ client/src/actions/countActions.js
@@ -3,7 +3,7 @@ import * as types from "./types";
 
 export const countSongs = () => async dispatch => {
   try {
-    const { data } = await axios.get("metadata/count/songs");
+    const { data } = await axios.get("/metadata/count/songs");
     dispatch({ type: types.COUNT_SONGS, payload: data });
   } catch (error) {
     console.log(error);
