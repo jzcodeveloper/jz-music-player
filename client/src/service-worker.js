254a0542371b29e434a191fe2cf6cb72226ebda8
Javier <javier_bislip@hotmail.com>
2019-08-31 13:26:35 -0400

    Added more manifest properties + Added custom service worker + Added fontawesome link

--- client/src/service-worker.js
+++ client/src/service-worker.js
@@ -15,7 +15,7 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 ///// DYNAMIC CACHING - FONTS /////
 ///////////////////////////////////
 workbox.routing.registerRoute(
-  /fontawesome/,
+  /fontawesome\.com/,
   new workbox.strategies.CacheFirst({
     cacheName: "fonts",
     cacheExpiration: { maxEntries: 1, maxAgeSeconds: 60 * 60 * 24 * 30 }
