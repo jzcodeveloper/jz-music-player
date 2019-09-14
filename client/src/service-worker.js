5194bdd1ff3a077a993a598eeb2598ddd529b75c
Javier <javier_bislip@hotmail.com>
2019-09-14 00:49:14 -0400

    Workbox migration

--- client/src/service-worker.js
+++ client/src/service-worker.js
@@ -17,7 +17,11 @@ workbox.routing.registerRoute(
   /.*fontawesome\.com.*/,
   new workbox.strategies.CacheFirst({
     cacheName: "fonts",
-    cacheExpiration: { maxAgeSeconds: 60 * 60 * 24 * 30 }
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 60 * 60 * 24 * 30
+      })
+    ]
   })
 );
 
@@ -28,22 +32,23 @@ workbox.routing.registerRoute(
   /\.(?:jpg|jpeg|png)$/,
   new workbox.strategies.CacheFirst({
     cacheName: "images",
-    cacheExpiration: { maxEntries: 20 }
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 20
+      })
+    ]
   })
 );
 
 ///////////////////////////
 ///// BACKGROUND SYNC /////
 ///////////////////////////
-const bgSyncPlugin = new workbox.backgroundSync.Plugin("sync", {
-  maxRetentionTime: 60 * 24, // According to the documentation, time here is in minutes
-  callbacks: {
-    queueDidReplay: e => console.log("Background Sync completed successfully!")
-  }
-});
-
 const networkWithBackgroundSync = new workbox.strategies.NetworkOnly({
-  plugins: [bgSyncPlugin]
+  plugins: [
+    new workbox.backgroundSync.Plugin("sync", {
+      maxRetentionTime: 60 * 24
+    })
+  ]
 });
 
 workbox.routing.registerRoute(/\/api\/.*/, networkWithBackgroundSync, "POST");
