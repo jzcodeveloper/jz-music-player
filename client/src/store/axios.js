9e07f34c9aa21dbf1b1d1446e93969691dc80a79
Javier <javier_bislip@hotmail.com>
2019-09-15 23:07:52 -0400

    Precached Font Awesome Fonts

--- client/src/store/axios.js
+++ client/src/store/axios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  /* baseURL: "https://jz-music-player.herokuapp.com/api" */
-  baseURL: "http://localhost:5001/api"
+  baseURL: "https://jz-music-player.herokuapp.com/api"
 });
 
 export default axiosInstance;
