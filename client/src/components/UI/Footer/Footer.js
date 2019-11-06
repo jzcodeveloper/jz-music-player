ae587514c276eb5561cf7ff8ec5647c948efc321
Javier <javier_bislip@hotmail.com>
2019-11-06 10:32:47 -0400

    Bold H1

--- client/src/components/UI/Footer/Footer.js
+++ client/src/components/UI/Footer/Footer.js
@@ -5,16 +5,36 @@ import classes from "./Footer.css";
 const Footer = () => (
   <footer className={classes.Footer}>
     <div className={classes.SocialIcons}>
-      <a className={classes.SocialLink} href="https://www.facebook.com">
+      <a
+        className={classes.SocialLink}
+        href="https://www.facebook.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <i className="fab fa-facebook"></i>
       </a>
-      <a className={classes.SocialLink} href="https://twitter.com">
+      <a
+        className={classes.SocialLink}
+        href="https://twitter.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <i className="fab fa-twitter"></i>
       </a>
-      <a className={classes.SocialLink} href="https://www.instagram.com">
+      <a
+        className={classes.SocialLink}
+        href="https://www.instagram.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <i className="fab fa-instagram"></i>
       </a>
-      <a className={classes.SocialLink} href="https://www.linkedin.com">
+      <a
+        className={classes.SocialLink}
+        href="https://www.linkedin.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <i className="fab fa-linkedin"></i>
       </a>
     </div>
