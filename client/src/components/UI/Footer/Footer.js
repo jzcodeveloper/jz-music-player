10381bfe5edfa46f3f804a39048439b84c0105d3
Javier <javier_bislip@hotmail.com>
2019-11-06 00:08:53 -0400

    Landing page

--- client/src/components/UI/Footer/Footer.js
+++ client/src/components/UI/Footer/Footer.js
@@ -4,7 +4,23 @@ import classes from "./Footer.css";
 
 const Footer = () => (
   <footer className={classes.Footer}>
-    Copyright &copy; {new Date().getFullYear()} JZ Music Player
+    <div className={classes.SocialIcons}>
+      <a className={classes.SocialLink} href="https://www.facebook.com">
+        <i className="fab fa-facebook"></i>
+      </a>
+      <a className={classes.SocialLink} href="https://twitter.com">
+        <i className="fab fa-twitter"></i>
+      </a>
+      <a className={classes.SocialLink} href="https://www.instagram.com">
+        <i className="fab fa-instagram"></i>
+      </a>
+      <a className={classes.SocialLink} href="https://www.linkedin.com">
+        <i className="fab fa-linkedin"></i>
+      </a>
+    </div>
+    <div className={classes.Copyright}>
+      Copyright &copy; {new Date().getFullYear()} JZ Music Player
+    </div>
   </footer>
 );
 
--- client/src/containers/Home/Home.js
+++ client/src/containers/Home/Home.js
@@ -22,21 +22,70 @@ const Home = () => {
 
   return (
     <div className={classes.Home}>
-      <div className={classes.Overlay}>
-        <div>
-          <p>
-            JZ Music Player
-            <br />
-            Listen to your favorite songs for free...
-            <br />
-            {count} songs currently available.
-          </p>
+      <div className={classes.Top}>
+        <div className={classes.Overlay}>
           <div>
-            <Link to="/register" className={classes.Register}>
-              Register Now
-            </Link>
-            <Link to="/login" className={classes.Login}>
-              Login
+            <h1>JZ Music Player</h1>
+            <p>
+              Listen to your favorite songs for free from any device. There are
+              currently {count} songs available.
+            </p>
+            <div>
+              <Link to="/register" className={classes.Register}>
+                Register Now
+              </Link>
+              <Link to="/login" className={classes.Login}>
+                Login
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className={classes.Middle}>
+        <div className={classes.MiddleTitle}>
+          <h1>What We Offer</h1>
+        </div>
+        <div className={classes.Services}>
+          <div className={classes.Service}>
+            <i className="fas fa-music"></i>
+            <h3>Audio Streaming</h3>
+            <p>High-quality audio streaming to any device over HTTPS.</p>
+          </div>
+          <div className={classes.Service}>
+            <i className="fas fa-mobile-alt"></i>
+            <h3>Listen Everywhere</h3>
+            <p>This app works on your computer, mobile or tablet.</p>
+          </div>
+          <div className={classes.Service}>
+            <i className="fas fa-headphones"></i>
+            <h3>Unlimited, Ad-free Music</h3>
+            <p>No ads, just listen to music.</p>
+          </div>
+          <div className={classes.Service}>
+            <i className="fas fa-hand-holding-usd"></i>
+            <h3>Premium Sounds Better</h3>
+            <p>Get ready for incredible sound quality.</p>
+          </div>
+          <div className={classes.Service}>
+            <i className="fas fa-download"></i>
+            <h3>Download Your Songs</h3>
+            <p>Download as many songs as you want.</p>
+          </div>
+          <div className={classes.Service}>
+            <i className="far fa-star"></i>
+            <h3>Offline Support</h3>
+            <p>Keep listening to music even when offline.</p>
+          </div>
+        </div>
+      </div>
+
+      <div className={classes.Bottom}>
+        <div className={classes.Overlay2}>
+          <div>
+            <h1>Why wait?</h1>
+            <Link to="/login" className={classes.Register}>
+              Start Now.
             </Link>
           </div>
         </div>
